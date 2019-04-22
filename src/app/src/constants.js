e4f8adbdee7bd4e9e1ef9248c4a72b5e3fe424ee
Jenny Fung <jfung@azavea.com>
2019-04-22 16:32:27 -0400

    Convert data to constants for best of fish roster
    Refs #19

--- src/app/src/constants.js
+++ src/app/src/constants.js
@@ -28,3 +28,124 @@ export const aboutProfiles = [
         "videoPath": "./media/00_About/schuylkill.mp4"
     }
 ];
+
+export const fishHighlights = [
+    {
+        "commonName": "American Shad",
+        "scientificName": "Alosa sapidissima",
+        "timestamp": 1118762520000,
+        "notes": "Three American shad dart through the ladder during their annual spawning migration",
+        "filename": "shad"
+    },
+    {
+        "filename": "quillback",
+        "commonName": "Quillback",
+        "scientificName": "Carpiodes cyprinus",
+        "timestamp": 1115046300000,
+        "notes": "This fish is named for its pointed dorsal fin — thought to be in the shape of a 'quill.'" 
+    },
+    {
+        "filename": "pumpkinseed",
+        "commonName": "Pumpkinseed",
+        "scientificName": "Lepomis gibbosus",
+        "timestamp": 1116184920000,
+        "notes": "This fish is a type of sunfish common to PA waters. Can you guess where its name comes from?"
+    },
+    {
+        "filename": "hybrid_striped_bass",
+        "commonName": "Hybrid Striped Bass",
+        "scientificName": "Morone saxatilis x Morone chrysops",
+        "timestamp": 1114074720000,
+        "notes": "Striped bass migrate upriver each year to spawn, much like the American shad."
+    },
+    {
+        "filename": "turtle",
+        "commonName": "Turtle",
+        "scientificName": "—",
+        "timestamp": 1114119720000,
+        "notes": "A turtle moves downstream through the ladder, carried along by the current."
+    },
+    {
+        "filename": "snake",
+        "commonName": "Snake",
+        "scientificName": "—",
+        "timestamp": 1115907780000,
+        "notes": "A 3-foot long river snake swims downstream. Some snakes can swim almost as well as fish!"
+    },
+    { 
+        "filename": "rainbow_trout",
+        "commonName": "Rainbow Trout",
+        "scientificName": "Oncorhynchus mykiss",
+        "timestamp": 1115870340000,
+        "notes": "A rainbow trout is shown just before midnight – captured in the act of feeding."
+    },
+    {
+        "filename": "carp",
+        "commonName": "Common Carp",
+        "scientificName": "Cyprinus carpio",
+        "timestamp": 1115826720000,
+        "notes": "Two enormous carp move through the ladder at night. The first carp is over three feet long!"
+    },
+    {
+        "filename": "river_otter",
+        "commonName": "River Otter",
+        "scientificName": "Lutra canadensis",
+        "timestamp": 1111371600000,
+        "notes": "River Otter swims through the fish ladder to star in a video."
+    },
+    {
+        "filename": "american_shad",
+        "commonName": "American Shad",
+        "scientificName": "Alosa sapidissima",
+        "timestamp": 1085515200000,
+        "notes": "An American shad swims upstream during the annual spawning migration."
+    },
+    {
+        "filename": "channel_catfish",
+        "commonName": "Channel Catfish",
+        "scientificName": "Ictalurus punctatus",
+        "timestamp": 1084417680000,
+        "notes": "Catfish group, traveling at night."
+    },
+    {
+        "filename": "common_carp",
+        "commonName": "Common Carp",
+        "scientificName": "Cyprinus carpio",
+        "timestamp": 1084512000000,
+        "notes": "A very large female carp, pregnant with eggs."
+    }, {
+        "filename": "quillback_carp",
+        "commonName": "Quillback and Common Carp",
+        "scientificName": "Carpiodes cyprinus and Cyprinus carpio",
+        "timestamp": 1084417860000,
+        "notes": "A quillback and a carp try to swim together upriver.",
+    },
+    {
+        "filename": "quillbacks",
+        "commonName": "Quillback",
+        "scientificName": "Carpiodes cyprinus",
+        "timestamp": 1084454640000,
+        "notes": "Three quillbacks swim by the viewing window."
+    },
+    {
+        "filename": "quillback_catfish",
+        "commonName": "Quillbacks and Channel Catfish",
+        "scientificName": "Carpiodes cyprinus and Ictalurus punctatus",
+        "timestamp": 1084461660000,
+        "notes": "A group of quillbacks swim through the fish ladder, followed by two catfish."
+    },
+    {
+        "filename": "striped_bass_hybrid",
+        "commonName": "Hybrid Striped Bass",
+        "scientificName": "Morone saxatilis x Morone chrysops",
+        "timestamp": 1084279080000,
+        "notes": "A very large, hybrid striped bass — an excellent specimen."
+    },
+    {
+        "filename": "white_perch",
+        "commonName": "White Perch",
+        "scientificName": "Morone americana",
+        "timestamp": 1084379160000,
+        "notes": "A white perch darts through the water."
+    }
+];
