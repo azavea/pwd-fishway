b8d27584a8d06d10469858000220b457826f4074
Jenny Fung <jfung@azavea.com>
2019-04-22 15:52:58 -0400

    Convert 'about' app copy to usable constants
    Refs #10

---
+++ src/app/src/constants.js
@@ -0,0 +1,30 @@
+export const aboutProfiles = [
+    {
+        "title": "About the Fishway",
+        "name": "",
+        "job": "",
+        "jobDescription": "The Fish Ladder at the Fairmount Dam was constructed in 1979. This animated 3-D video explains how the Fish Ladder works, and how it helps fish to migrate, and survive.",
+        "videoPath": "./media/00_About/overview.mp4"
+    },
+    {
+        "title": "Meet the Biologist",
+        "name": "Joe Perillo",
+        "job": "Aquatic Biologist, PWD",
+        "jobDescription": "Studies fish and the ecology of local streams. Records data about fish populations, tracks migration of species, and reports on the general health of local watersheds.",
+        "videoPath": "./media/00_About/biologist.mp4"
+    },
+    {
+        "title": "Meet the Engineer",
+        "name": "Chad Pindar",
+        "job": "Water Resources Engineer, PWD",
+        "jobDescription": "Prepares computer models of rivers and streams, works on watershed restoration projects, and develops watershed management plans.",
+        "videoPath": "./media/00_About/engineer.mp4"
+    },
+    {
+        "title": "Dams Along the Schuylkill",
+        "name": "Glenn McKenzie",
+        "job": "Engineer, Army Corps of Engineers",
+        "jobDescription": "Works to construct and maintain fish ladders and other environmental projects for area waterways and rivers.",
+        "videoPath": "./media/00_About/schuylkill.mp4"
+    }
+];
