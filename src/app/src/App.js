66816819638fd2212e06cb443bb6239dc8ea02ea
Jenny Fung <jfung@azavea.com>
2019-04-23 12:45:22 -0400

    Add navbar tabs
    Refs #8

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Screensaver from './Screensaver';
+import Navbar from './Navbar';
 
 class App extends Component {
     render() {
         return (
             <div className='App'>
+                <Navbar />
                 <Screensaver />
             </div>
         );
