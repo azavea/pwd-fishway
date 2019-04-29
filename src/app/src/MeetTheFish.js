6ac660390a2440ace843c86f99ff6c7b0b04253f
Jenny Fung <jfung@azavea.com>
2019-04-29 16:18:07 -0400

    Add meet the fish component
    Refs #14

---
+++ src/app/src/MeetTheFish.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function MeetTheFish() {
+    return (
+        <div className='meet-the-fish'>
+            <div className='header'>
+                <h3>Meet the Fish</h3>
+                <div className='header__description'>
+                    Over 48 different species of fish live in the waterways of
+                    Philadelphia. Here are 24 of the most popular species.
+                    <h4>Be an aquatic biologist and know your fish!</h4>
+                </div>
+            </div>
+        </div>
+    );
+}
