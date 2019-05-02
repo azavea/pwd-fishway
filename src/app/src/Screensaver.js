aa393c96f9d371273477d57e6935032022f93cf8
alexelash <alash@azavea.com>
2019-05-02 13:43:20 -0400

    Use new Rebass components on Screensaver component

--- src/app/src/Screensaver.js
+++ src/app/src/Screensaver.js
@@ -1,48 +1,43 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { func } from 'prop-types';
-import { hideScreensaver } from './actions';
-import styled from 'styled-components';
+import React from "react";
+import { connect } from "react-redux";
+import { func } from "prop-types";
+import { hideScreensaver } from "./actions";
+import styled from "styled-components";
+import { Flex, Box, Heading, Button } from "rebass";
 
-const StyledScreensaver = styled.div`
-    display: flex;
-    height: 100%;
-    flex-direction: column;
-    align-items: center;
-
-    p {
-        font-size: 2rem;
-        margin-top: 40vh;
-        text-align: center;
-    }
+const StyledScreensaver = styled(Flex)`
+  text-align: center;
 `;
 
 const Screensaver = props => {
-    const { dispatch } = props;
+  const { dispatch } = props;
 
-    return (
-        <StyledScreensaver
-            className='screensaver'
-            onClick={() => dispatch(hideScreensaver())}
-        >
-            <p>
-                Learn how the fish ladder helps fish populations and whether you
-                have what it takes to be an aquatic biologist.
-            </p>
-            <button type='button' className='screensaver__button'>
-                Let's go!
-            </button>
-        </StyledScreensaver>
-    );
+  return (
+    <StyledScreensaver
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      className="screensaver"
+      onClick={() => dispatch(hideScreensaver())}
+    >
+      <Box width={1 / 2}>
+        <Heading as="h1" fontSize="3" mb="4">
+          Learn how the fish ladder helps fish populations and whether you have
+          what it takes to be an aquatic biologist.
+        </Heading>
+        <Button className="screensaver__button">Let's go!</Button>
+      </Box>
+    </StyledScreensaver>
+  );
 };
 
 Screensaver.propTypes = {
-    dispatch: func.isRequired,
+  dispatch: func.isRequired
 };
 
 function mapStateToProps(state) {
-    return {
-        dispatch: state.dispatch,
-    };
+  return {
+    dispatch: state.dispatch
+  };
 }
 export default connect(mapStateToProps)(Screensaver);
