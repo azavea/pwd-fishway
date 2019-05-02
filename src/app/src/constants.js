9677f51b22c6a22accfbdf5d7cc0aeee32e780f6
Michael Maurizi <mmaurizi@azavea.com>
2019-05-02 13:45:45 -0400

    Add About carousel content
    Connects to #12

--- src/app/src/constants.js
+++ src/app/src/constants.js
@@ -3,7 +3,7 @@ export const aboutProfiles = [
         title: 'About the Fishway',
         name: '',
         job: '',
-        jobDescription:
+        description:
             'The Fish Ladder at the Fairmount Dam was constructed in 1979. This animated 3-D video explains how the Fish Ladder works, and how it helps fish to migrate, and survive.',
         videoPath: './media/00_About/overview.mp4',
     },
@@ -11,7 +11,7 @@ export const aboutProfiles = [
         title: 'Meet the Biologist',
         name: 'Joe Perillo',
         job: 'Aquatic Biologist, PWD',
-        jobDescription:
+        description:
             'Studies fish and the ecology of local streams. Records data about fish populations, tracks migration of species, and reports on the general health of local watersheds.',
         videoPath: './media/00_About/biologist.mp4',
     },
@@ -19,7 +19,7 @@ export const aboutProfiles = [
         title: 'Meet the Engineer',
         name: 'Chad Pindar',
         job: 'Water Resources Engineer, PWD',
-        jobDescription:
+        description:
             'Prepares computer models of rivers and streams, works on watershed restoration projects, and develops watershed management plans.',
         videoPath: './media/00_About/engineer.mp4',
     },
@@ -27,7 +27,7 @@ export const aboutProfiles = [
         title: 'Dams Along the Schuylkill',
         name: 'Glenn McKenzie',
         job: 'Engineer, Army Corps of Engineers',
-        jobDescription:
+        description:
             'Works to construct and maintain fish ladders and other environmental projects for area waterways and rivers.',
         videoPath: './media/00_About/schuylkill.mp4',
     },
