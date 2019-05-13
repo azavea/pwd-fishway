070886dce3e837206f4b6240cb57afa1d07ec672
Jenny Fung <fungjj92@gmail.com>
2019-05-13 15:04:02 -0400

    Add quiz guess component and quiz fish prop type
    Add a lightweight demo in the Quiz component that sets up guessing
    
    Refs #41

--- src/app/src/components/Quiz.js
+++ src/app/src/components/Quiz.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Flex, Box } from 'rebass';
+import { Flex, Box, Button } from 'rebass';
 import { themeGet } from 'styled-system';
 
 import QuizNavbar from './QuizNavbar';
 import QuizGuess from './QuizGuess';
 import Sidebar from './Sidebar';
 
-import { QUIZ_FISH } from '../util/constants';
+import { QUIZ_FISH, GUESS_MESSAGE } from '../util/constants';
 
 const QuizContainer = styled(Flex)`
     text-align: center;
@@ -15,13 +15,32 @@ const QuizContainer = styled(Flex)`
 `;
 
 const Quiz = props => {
+    const [answer] = useState(QUIZ_FISH[0]);
+    const [guess, setGuess] = useState(null);
+
+    function checkGuess(fish) {
+        setGuess(fish);
+        setTimeout(() => setGuess(null), GUESS_MESSAGE);
+    }
+
+    const quizState = guess ? (
+        <QuizGuess answer={answer} guess={guess} />
+    ) : (
+        <QuizContainer>
+            <Sidebar />
+            <Button onClick={event => checkGuess(QUIZ_FISH[0])}>
+                Guess correctly
+            </Button>
+            <Button onClick={event => checkGuess(QUIZ_FISH[1])}>
+                Guess incorrectly
+            </Button>
+        </QuizContainer>
+    );
+
     return (
         <Box>
             <QuizNavbar dispatch={props.dispatch} />
-            <QuizContainer>
-                <Sidebar />
-                <QuizGuess answer={QUIZ_FISH[0]} guess={QUIZ_FISH[0]} />
-            </QuizContainer>
+            {quizState}
         </Box>
     );
 };
--- src/app/src/components/QuizGuess.js
+++ src/app/src/components/QuizGuess.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Box } from 'rebass';
+import { Flex } from 'rebass';
+import { objectOf } from 'prop-types';
 import { themeGet } from 'styled-system';
 
-const StyledQuizGuess = styled(Box)`
+import Text from './Text';
+
+import { QuizFish } from '../util/QuizFish';
+
+const StyledQuizGuess = styled(Flex)`
     text-align: center;
     background: ${themeGet('colors.teals.0')};
+    flex-direction: column;
+`;
+
+const Answer = styled(Flex)`
+    flex-direction: column;
+    text-align: center;
 `;
 
 const QuizGuess = props => {
@@ -18,18 +29,26 @@ const QuizGuess = props => {
 
     return (
         <StyledQuizGuess>
-            {message}
-            <img src={answer.picturePath} alt='Illustration of the fish' />
-            {answer.commonName}
-            {answer.scientificName}
-            {answer.hint}
+            <Answer>
+                {message}
+                <img src={answer.picturePath} alt='Illustration of the fish' />
+                <Text as='h2' variant='base'>
+                    {answer.commonName}
+                </Text>
+                <Text as='h3' variant='xSmall'>
+                    {answer.scientificName}
+                </Text>
+            </Answer>
+            <Text as='h2' variant='xSmall'>
+                {answer.hint}
+            </Text>
         </StyledQuizGuess>
     );
 };
 
 QuizGuess.propTypes = {
-    answer: Object.isRequired,
-    guess: Object.isRequired,
+    answer: objectOf(QuizFish).isRequired,
+    guess: objectOf(QuizFish).isRequired,
 };
 
 export default QuizGuess;
