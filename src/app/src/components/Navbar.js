113610780f10e8e973bdd38f53980a715ab40927
alexelash <alash@azavea.com>
2019-05-21 15:31:08 -0400

    Style navbar

--- src/app/src/components/Navbar.js
+++ src/app/src/components/Navbar.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
+import { Heading } from './custom-styled-components';
 import styled from 'styled-components';
+import { themeGet } from 'styled-system';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import About from './About';
 import SeeTheFishway from './SeeTheFishway';
@@ -12,6 +15,58 @@ const StyledTabs = styled(Tabs)`
     width: 100%;
     display: flex;
     justify-content: space-evenly;
+    align-items: center;
+    height: 5rem;
+    border-bottom: 1px solid ${themeGet('colors.teals.1')};
+    background: ${themeGet('colors.teals.3')};
+
+    .nav-item {
+        text-decoration: none;
+        position: relative;
+        display: flex;
+        align-items: flex-end;
+        transition: opacity 0.75s ease-in;
+
+        span {
+            margin-bottom: 0;
+            line-height: 1.5;
+        }
+
+        &::after {
+            content: '';
+            display: block;
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: -27px;
+            height: 2px;
+            background: right center / 250%
+                linear-gradient(
+                    to right,
+                    ${themeGet('colors.lightblues.0')} 0 20%,
+                    ${themeGet('colors.lightblues.1')} 25% 50%,
+                    transparent 50% 100%
+                );
+            border-radius: 2px;
+            opacity: 0;
+            transition: background-position 0.25s ease-in,
+                opacity 0.25s 0.05s ease-in;
+        }
+    }
+
+    .nav-item:focus {
+        outline: none;
+    }
+
+    .active span,
+    .active svg {
+        opacity: 1;
+    }
+
+    .active::after {
+        background-position: left center;
+        opacity: 1;
+    }
 `;
 
 const StyledNavbar = styled.div`
@@ -19,21 +74,68 @@ const StyledNavbar = styled.div`
 `;
 
 const Navbar = props => {
+    const titles = {
+        about: (
+            <Heading as='span' variant='xSmall' opacity='0.8'>
+                <FontAwesomeIcon
+                    icon={['fas', 'info-circle']}
+                    pull='left'
+                    opacity='0.8'
+                    size='lg'
+                />
+                About
+            </Heading>
+        ),
+        seeTheFishway: (
+            <Heading as='span' variant='xSmall' opacity='0.8'>
+                <FontAwesomeIcon
+                    icon={['fas', 'video']}
+                    pull='left'
+                    opacity='0.8'
+                    size='lg'
+                />
+                See the Fishway
+            </Heading>
+        ),
+        meetTheFish: (
+            <Heading as='span' variant='xSmall' opacity='0.8'>
+                <FontAwesomeIcon
+                    icon={['fas', 'fish']}
+                    pull='left'
+                    opacity='0.8'
+                    size='lg'
+                />
+                Meet the Fish
+            </Heading>
+        ),
+        testYourSkills: (
+            <Heading as='span' variant='xSmall' opacity='0.8'>
+                <FontAwesomeIcon
+                    icon={['far', 'bullseye-pointer']}
+                    pull='left'
+                    opacity='0.8'
+                    size='lg'
+                />
+                Test your Skills
+            </Heading>
+        ),
+    };
+
     return (
         <StyledNavbar hide={props.isQuizVisible ? 'none' : 'visible'}>
             {/* unmountOnExit is used to ensure that videos restart when
             switching from About tab to another tab and back again */}
             <StyledTabs defaultActiveKey='about' unmountOnExit={true}>
-                <Tab eventKey='about' title='About'>
+                <Tab eventKey='about' title={titles.about}>
                     <About />
                 </Tab>
-                <Tab eventKey='see' title='See the Fishway'>
+                <Tab eventKey='see' title={titles.seeTheFishway}>
                     <SeeTheFishway />
                 </Tab>
-                <Tab eventKey='meet' title='Meet the Fish'>
+                <Tab eventKey='meet' title={titles.meetTheFish}>
                     <MeetTheFish />
                 </Tab>
-                <Tab eventKey='test' title='Test your Skills'>
+                <Tab eventKey='test' title={titles.testYourSkills}>
                     <QuizHome />
                 </Tab>
             </StyledTabs>
