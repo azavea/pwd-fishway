51956d39f5aef65d094eb563f9a092a9ba4f0f9b
Michael Maurizi <mmaurizi@azavea.com>
2019-05-28 18:54:29 -0400

    Add component for quiz options that tracks state of the current question

--- src/app/src/components/Quiz.js
+++ src/app/src/components/Quiz.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Flex, Box, Button } from 'rebass';
+import { Box, Flex } from 'rebass';
 import { themeGet } from 'styled-system';
 
 import QuizNavbar from './QuizNavbar';
 import QuizGuess from './QuizGuess';
+import QuizQuestion from './QuizQuestion';
 import QuizSidebar from './QuizSidebar';
 
 import { QUIZ_FISH, GUESS_MESSAGE_TIME } from '../util/constants';
@@ -20,26 +21,26 @@ const QuizBody = styled(Box)`
 
 const Quiz = props => {
     const [answer] = useState(QUIZ_FISH[0]);
-    const [guess, setGuess] = useState(null);
+    const [results, setResults] = useState(null);
 
-    function checkGuess(fish) {
-        setGuess(fish);
-        setTimeout(() => setGuess(null), GUESS_MESSAGE_TIME);
+    function checkResults(results) {
+        setResults(results);
+        setTimeout(() => setResults(null), GUESS_MESSAGE_TIME);
     }
 
+    const correctGuess = results !== null && results.numWrong < 2;
     const quizState =
-        guess !== null ? (
-            <QuizGuess answer={answer} guess={guess} />
+        results !== null ? (
+            <QuizGuess answer={answer} correct={correctGuess} />
         ) : (
             <QuizContainer>
-                <QuizSidebar fish={QUIZ_FISH[0]} />
-                <QuizBody>
-                    <Button onClick={event => checkGuess(QUIZ_FISH[0])}>
-                        Guess correctly
-                    </Button>
-                    <Button onClick={event => checkGuess(QUIZ_FISH[1])}>
-                        Guess incorrectly
-                    </Button>
+                <QuizSidebar fish={answer} width={1 / 5} />
+                <QuizBody width={4 / 5}>
+                    <QuizQuestion
+                        answer={answer}
+                        choices={[QUIZ_FISH[0], QUIZ_FISH[1], QUIZ_FISH[2]]}
+                        onDone={checkResults}
+                    />
                 </QuizBody>
             </QuizContainer>
         );
