659d4debcab42cf7d78d943d17bddcaf48802582
alexelash <alash@azavea.com>
2019-08-21 12:29:32 -0400

    Add fish swimming animation to quiz and screensaver

--- src/app/src/components/QuizHome.js
+++ src/app/src/components/QuizHome.js
@@ -6,6 +6,9 @@ import styled from 'styled-components';
 import { themeGet } from 'styled-system';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import Lottie from 'react-lottie';
+import animationData from '../media/lotties/fish_swimming.json';
+
 import { Heading, Text, Button } from './custom-styled-components';
 import {
     showQuiz,
@@ -25,6 +28,8 @@ const StyledQuizHome = styled(Flex)`
 
 const StyledFinalQuizState = styled(StyledQuizHome)`
     margin-top: 15%;
+    z-index: 1;
+    position: relative;
 `;
 
 const StyledScoreIntro = styled(Text)`
@@ -45,6 +50,16 @@ const StyledScore = styled(Heading)`
 
 const StyledButton = styled(Button)`
     margin: 1rem;
+`;
+
+const StyledFishSwiming = styled(Box)`
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    z-index: 0;
+    pointer-events: none;
 `;
 
 class QuizHome extends Component {
@@ -68,6 +83,25 @@ class QuizHome extends Component {
         const medallion = (
             <QuizMedallion value={bonusPoints} isFinalScorePage={true} />
         );
+
+        const defaultOptions = {
+            loop: true,
+            autoplay: true,
+            animationData: animationData,
+            rendererSettings: {
+                preserveAspectRatio: 'xMidYMid slice',
+                clearCanvas: true,
+                progressiveLoad: true,
+            },
+        };
+
+        const FishSwimming = () => {
+            return (
+                <StyledFishSwiming>
+                    <Lottie options={defaultOptions} />
+                </StyledFishSwiming>
+            );
+        };
 
         const quizHomeState = quizScore ? (
             <StyledFinalQuizState>
@@ -95,7 +129,7 @@ class QuizHome extends Component {
                 </Text>
                 <Flex alignItems='baseline'>
                     <StyledButton
-                        mt='compact'
+                        mt='normal'
                         variant='secondary'
                         onClick={() => {
                             dispatch(setActiveTab(MEET));
@@ -105,7 +139,7 @@ class QuizHome extends Component {
                         Meet The Fish
                     </StyledButton>
                     <StyledButton
-                        mt='compact'
+                        mt='normal'
                         onClick={() => dispatch(clearQuizScore())}
                     >
                         Try again
@@ -114,28 +148,33 @@ class QuizHome extends Component {
                 </Flex>
             </StyledFinalQuizState>
         ) : (
-            <StyledFinalQuizState>
-                <Box width={880} mb='4'>
-                    <Heading as='h1' variant='medium'>
-                        Test your skills
-                    </Heading>
-                    <Text as='p' variant='large'>
-                        Aquatic biologists monitor the live video feed captured
-                        by the fishway camera to identify and document each fish
-                        that moves through the fishway. This can be tough,
-                        because fish swim fast and are hard to see!
-                    </Text>
-                    <Text as='p'>
-                        Watch the video clip of fish moving through the fishway
-                        and match it to the correct species of fish. Work
-                        quickly and carefully to get the highest score!
-                    </Text>
-                </Box>
-                <Button mt='compact' onClick={() => dispatch(showQuiz())}>
-                    Play
-                </Button>
-            </StyledFinalQuizState>
+            <>
+                <StyledFinalQuizState>
+                    <Box width={880} mb='4'>
+                        <Heading as='h1' variant='medium'>
+                            Test your skills
+                        </Heading>
+                        <Text as='p' variant='large'>
+                            Aquatic biologists monitor the live video feed
+                            captured by the fishway camera to identify and
+                            document each fish that moves through the fishway.
+                            This can be tough, because fish swim fast and are
+                            hard to see!
+                        </Text>
+                        <Text as='p'>
+                            Watch the video clip of fish moving through the
+                            fishway and match it to the correct species of fish.
+                            Work quickly and carefully to get the highest score!
+                        </Text>
+                    </Box>
+                    <Button mt='normal' onClick={() => dispatch(showQuiz())}>
+                        Play
+                    </Button>
+                </StyledFinalQuizState>
+                <FishSwimming />
+            </>
         );
+
         return <StyledQuizHome>{quizHomeState}</StyledQuizHome>;
     }
 }
--- src/app/src/components/Screensaver.js
+++ src/app/src/components/Screensaver.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { connect } from 'react-redux';
 import { func } from 'prop-types';
 import styled from 'styled-components';
 import { Flex, Box } from 'rebass';
 import { Heading, Button } from './custom-styled-components';
+import { themeGet } from 'styled-system';
+
+import Lottie from 'react-lottie';
+import animationData from '../media/lotties/fish_swimming.json';
 
 import { hideScreensaver } from '../actions';
 
@@ -14,12 +18,79 @@ const StyledScreensaver = styled(Flex)`
     justify-content: center;
 `;
 
+const StyledTextContainer = styled(Box)`
+    z-index: 2;
+    position: relative;
+    text-shadow: 0px 0px 8px rgba(0, 0, 0, 0.5);
+`;
+
+const StyledButton = styled(Button)`
+    font-size: ${themeGet('fontSizes.3')};
+    padding: ${themeGet('space.normal')} 5rem;
+`;
+
+const StyledFishSwiming = styled(Box)`
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    z-index: 0;
+    pointer-events: none;
+
+    .fish-swimming {
+        opacity: 0;
+        transition: opacity 2s;
+    }
+
+    .show .fish-swimming {
+        opacity: 1;
+    }
+`;
+
 const Screensaver = props => {
     const { dispatch } = props;
 
+    const lottieEl = useRef(null);
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: false,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice',
+            clearCanvas: true,
+            progressiveLoad: true,
+            className: 'fish-swimming',
+        },
+    };
+
+    const eventListeners = [
+        {
+            eventName: 'loaded_images',
+            callback: () => {
+                console.log(lottieEl.current.anim.wrapper);
+                lottieEl.current.anim.wrapper.classList.add('show');
+                lottieEl.current.anim.play();
+            },
+        },
+    ];
+
+    const FishSwimming = () => {
+        return (
+            <StyledFishSwiming>
+                <Lottie
+                    options={defaultOptions}
+                    eventListeners={eventListeners}
+                    ref={lottieEl}
+                />
+            </StyledFishSwiming>
+        );
+    };
+
     return (
         <StyledScreensaver onClick={() => dispatch(hideScreensaver())}>
-            <Box width={4 / 5}>
+            <StyledTextContainer width={4 / 5}>
                 <Heading
                     as='h1'
                     variant='medium'
@@ -31,8 +102,10 @@ const Screensaver = props => {
                     Learn how fishways help fish reach their spawning habitats
                     and test your fish identification skills!
                 </Heading>
-                <Button>Dive in!</Button>
-            </Box>
+                <StyledButton mt='normal'>Dive in!</StyledButton>
+            </StyledTextContainer>
+
+            <FishSwimming />
         </StyledScreensaver>
     );
 };
